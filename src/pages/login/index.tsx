5e687883c7a0c425ad7356c5ebf51b648f93f69a
Naveenchandar <naveenchandece@gmail.com>
2025-07-01 23:27:02 +0530

    Add logo to login page and update primary color
    Added 'tintu_logo.png' and displayed it on the login page. Updated the --primary color variable in index.css to a new shade of blue.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../../components/ui/button"
 import { Input } from "../../components/ui/input"
+import Image1 from '../../assets/images/tintu_logo.png';
 
 export const LoginPage = () => {
     return (
@@ -10,9 +11,10 @@ export const LoginPage = () => {
             <div className="min-h-screen flex items-center justify-center">
                 <div className="flex flex-col items-center justify-center gap-4 w-full max-w-md">
                     <h1 className="text-2xl">Welcome Back</h1>
+                    <img src={Image1} alt="Chatbot Logo" />
                     <Input className="rounded-full min-h-[3rem] w-[20rem]" placeholder="Enter email" />
                     <Button className="rounded-full w-[20rem] min-h-[3rem]" variant='default'>Continue</Button>
-                    <p>Don't have an account? 
+                    <p>Don't have an account?
                         <a href="/signup" className="text-blue-500 hover:underline">{' '}Sign Up</a>
                     </p>
                 </div>
