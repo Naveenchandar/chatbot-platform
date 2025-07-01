60767011b010b771b03cc442ff7d9a6c82510126
Naveenchandar <naveenchandece@gmail.com>
2025-07-01 22:32:30 +0530

    Update primary color and adjust login page UI
    Changed the --primary CSS variable to a specific hex color (#1789f1) for improved color consistency. Updated the login page to explicitly set the Button variant and adjusted spacing in the sign-up link.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,9 +11,9 @@ export const LoginPage = () => {
                 <div className="flex flex-col items-center justify-center gap-4 w-full max-w-md">
                     <h1 className="text-2xl">Welcome Back</h1>
                     <Input className="rounded-full min-h-[3rem] w-[20rem]" placeholder="Enter email" />
-                    <Button className="rounded-full w-[20rem] min-h-[3rem]">Continue</Button>
+                    <Button className="rounded-full w-[20rem] min-h-[3rem]" variant='default'>Continue</Button>
                     <p>Don't have an account? 
-                        <a href="/signup" className="text-blue-500 hover:underline">Sign Up</a>
+                        <a href="/signup" className="text-blue-500 hover:underline">{' '}Sign Up</a>
                     </p>
                 </div>
             </div>
