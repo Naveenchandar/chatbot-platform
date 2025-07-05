713c23a557a2af66a762e9dde11c745663d4a9a6
Naveenchandar <naveenchandece@gmail.com>
2025-07-05 14:51:49 +0530

    Update login page UI and text
    Commented out the Chatbot heading, updated the main heading to 'Say Hi to Tintu...', and adjusted the logo image size. These changes improve the login page's appearance and branding.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,13 +5,13 @@ import Image1 from '../../assets/images/tintu_logo.png';
 export const LoginPage = () => {
     return (
         <section className="px-8">
-            <div className="flex justify-start">
+            {/* <div className="flex justify-start">
                 <h2 className="text-xl">Chatbot</h2>
-            </div>
+            </div> */}
             <div className="min-h-screen flex items-center justify-center">
                 <div className="flex flex-col items-center justify-center gap-4 w-full max-w-md">
-                    <h1 className="text-2xl">Welcome Back</h1>
-                    <img src={Image1} alt="Chatbot Logo" />
+                    <h1 className="text-xl">Say Hi to Tintu...</h1>
+                    <img src={Image1} alt="Chatbot Logo" className="h-[300px]" />
                     <Input className="rounded-full min-h-[3rem] w-[20rem]" placeholder="Enter email" />
                     <Button className="rounded-full w-[20rem] min-h-[3rem]" variant='default'>Continue</Button>
                     <p>Don't have an account?
