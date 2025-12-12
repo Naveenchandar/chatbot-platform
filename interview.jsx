eb527c9b87fff9b881c739c352e156f0d960e80d
Naveenchandar <naveenchandece@gmail.com>
2025-12-12 09:49:21 +0530

    interview react

---
+++ interview.jsx
@@ -0,0 +1,9 @@
+// how to secure the web applications
+performance optimization
+SPA vs MPA
+SSR
+next js pros
+react error boundary
+microfrontend
+what module federation does in microfrontend
+hydration
